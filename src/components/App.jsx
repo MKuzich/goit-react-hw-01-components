cd70432cdc79b45813de6984d3d114fdb81d1513
Mykhailo Kuzich <timmkedth@gmail.com>
2022-07-07 19:49:44 +0300

    reformatted by prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,18 @@ import data from 'data.json';
 import friends from 'friends.json';
 import transactions from 'transactions.json';
 
-
 export const App = () => {
   return (
     <div>
-      <Profile username={user.username}
+      <Profile
+        username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats} />
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
   );
--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,33 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StatsCard = styled.section`
-    margin-top: 40px;
-    margin-right: auto;
-    margin-left: auto;
-    width: 600px;
-    border-radius: 8px;
-    overflow: hidden;
-    border: 2px #70757A solid;
+  margin-top: 40px;
+  margin-right: auto;
+  margin-left: auto;
+  width: 600px;
+  border-radius: 8px;
+  overflow: hidden;
+  border: 2px #70757a solid;
 `;
 export const Title = styled.h2`
-    text-align: center;
+  text-align: center;
 `;
 export const Stats = styled.ul`
-    display: flex;
+  display: flex;
 `;
 export const StatsItem = styled.li`
-    display: flex;
-    flex-direction: column;
-    padding: 32px 0;
-    justify-content: space-around;
-    align-items: center;
-    width: ${props => (100 / props.quantity)}%;
+  display: flex;
+  flex-direction: column;
+  padding: 32px 0;
+  justify-content: space-around;
+  align-items: center;
+  width: ${props => 100 / props.quantity}%;
 `;
 export const Label = styled.span`
-    color: #ffffff;
+  color: #ffffff;
 `;
 export const Value = styled.span`
-    color: #ffffff;
-    font-weight: 700;
-    font-size: 20px;
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 20px;
 `;
